317d7322f19b1ba8c2f73312247a94bd974e0b85
ethan <ethanmannette@gmail.com>
2015-08-10 11:44:39 -0600

    Added functionality to button. Currently calculates a 20% tip

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 // console.log('test');
+
+document.getElementById('tip').addEventListener('click', function() {
+  // console.log('TIPS!!');
+  var tipDone = parseFloat(document.getElementById('bill').value) * 0.2;
+
+  console.log(tipDone.toFixed(2));
+
+  document.getElementById('tipAmount').innerHTML = "";
+
+  var textNode = document.createTextNode("You should tip $" + tipDone.toFixed(2));
+
+  document.getElementById('tipAmount').appendChild(textNode);
+
+
+});
