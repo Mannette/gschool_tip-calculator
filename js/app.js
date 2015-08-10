18daa703f2389bb03bbf7636c164321e53c37be8
ethan <ethanmannette@gmail.com>
2015-08-10 12:53:24 -0600

    Calculator now works with or without a dollar sign. Works with decimals.

--- js/app.js
+++ js/app.js
@@ -2,20 +2,28 @@
 
 document.getElementById('tip').addEventListener('click', function() {
   // console.log('TIPS!!');
-  var tipDone = parseFloat(document.getElementById('bill').value) * 0.2;
 
-  if (isNaN(tipDone)) {
-    alert('Please enter a valid amount!');
+  var tips = document.getElementById('bill').value;
+  // var tipDone = parseFloat(tips) * 0.2;
+
+  var newArray = tips.split('');
+
+  if (newArray[0] === "$") {
+    newArray.splice(0, 1);
+    var finalNum = parseFloat(newArray.join('') * 0.2);
+
+    if (isNaN(finalNum)) {
+      alert('Please enter a valid amount!');
+    }
+    else {
+      console.log(finalNum.toFixed(2));
+
+      document.getElementById('tipAmount').innerHTML = "";
+
+      var textNode = document.createTextNode("You should tip $" + finalNum.toFixed(2));
+
+      document.getElementById('tipAmount').appendChild(textNode);
+    }
   }
-  else {
-    console.log(tipDone.toFixed(2));
-
-    document.getElementById('tipAmount').innerHTML = "";
-
-    var textNode = document.createTextNode("You should tip $" + tipDone.toFixed(2));
-
-    document.getElementById('tipAmount').appendChild(textNode);
-  }
-
 
 });
